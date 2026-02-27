1833893b4f0a38decf53864fbd3b40783b36200f
Amber Shukla <ambershukla@Ambers-MacBook-Air.local>
2026-02-27 11:24:10 +0530

    welcome message

---
+++ src/com/palindromechecker/uc2/HardcodedPalindrome.java
@@ -0,0 +1,22 @@
+package com.palindromechecker.uc2;
+
+public class HardcodedPalindrome {
+
+   
+    public static void printResult() {
+        
+        String word = "racecar";
+
+        System.out.println("--------------------------------------------");
+        System.out.println("UC2: Hardcoded Palindrome Result");
+        System.out.println("--------------------------------------------");
+        System.out.println("String  : \"" + word + "\"");
+        System.out.println("Result  : \"" + word + "\" is a palindrome.");
+        System.out.println("--------------------------------------------");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printResult();
+    }
+}
